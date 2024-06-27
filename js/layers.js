f938022f5013ce94a4aa62c5ff49124b7ac90035
richardgrechko <162537734+richardgrechko@users.noreply.github.com>
2024-06-27 16:33:47 +0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -32,9 +32,11 @@ addLayer("a", {
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
     resource: "a", // Name of prestige currency
     resourceEN: "a", // The second name of prestige currency ( If you open otherLanguageMod )
-    baseResource: "points", 
-    baseAmount() {return player.points}, 
+    baseResource: "points", // Name of resource prestige is based on
+    baseResourceEN: "points", // The second name of resource prestige is based on ( If you open otherLanguageMod )
+    baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 0.9, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
         return mult
@@ -47,4 +49,24 @@ addLayer("a", {
         {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
     layerShown(){return true},
+})
+addLayer("2layer small", {// Add a * small* to generate a slightly different layer
+    name: "sideLayer2",
+    position: -1,
+    row: 27,
+    symbol() {return (options.ch || modInfo.languageMod==false) ? '↓ 层级 2 ↓' : '↓ layer 2 ↓'},
+    symbolEN() {return (options.ch || modInfo.languageMod==false) ? '↓ 层级 2 ↓' : '↓ layer 2 ↓'},
+    nodeStyle: {"font-size": "15px", "text-center": "center", "height": "30px"},
+    startData() { return {
+        unlocked: true,
+        small: true,
+        points: new Decimal(0),// This actually does nothing, but you have to write this. (Unless you want add something in this layer. #Todo, might change that later.)
+    }},
+    color: "#fefefe",
+    type: "none",
+    tooltip(){return false},
+    layerShown(){return layerDisplayTotal(['p'])},// If any layer in the array is unlocked, it will returns true. Otherwise it will return false.
+	tabFormat: [
+        ["display-text", function() { return getPointsDisplay() }]
+    ],
 })
