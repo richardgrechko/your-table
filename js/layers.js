dce0ff1cb5aa8c4c81a7a0fea0d0bf991ea4a199
richardgrechko <162537734+richardgrechko@users.noreply.github.com>
2024-06-29 07:16:45 +0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -143,6 +143,13 @@ addLayer("b", {
 			descriptionEN: "Auto buy a upgrades",
 			cost: new Decimal(100),
 		},
+		21: {
+			title: "更多乘數",
+			titleEN: "More Multipliers",
+			description: "獲得基於積分的a<br>現在：x{{player.points.log(10)}}</br>",
+			descriptionEN: "Gain a based on points.<br>Currently: x{{player.points.log(10)}}</br>",
+			cost: new Decimal(100),
+		},
 	},
 	passiveGeneration() { return player.c.points.gte(10) },
     layerShown(){return true},
